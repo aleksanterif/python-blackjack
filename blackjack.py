24258f65cd76997f5ef5e2f86968b75a88962bbb
aleksanterif <aleksanteri.fagerholm@gmail.comgit config --global user.email aleksanteri.fagerholm@gmail.com>
2020-05-10 11:25:06 +0300

    initial logic for how the black should be structured

--- blackjack.py
+++ blackjack.py
@@ -1,1 +1,19 @@
-print("hello world")
+#random will offer me a great way to randomly select cards
+import random
+
+ # Player cards are an empty array in the beginning
+player_cards = []
+ # Dealer cards are an empty array in the beginning
+dealer_cards = []
+
+# object in which I define the values of the cards
+# I need a function to deal the cards
+# Other funcion to display the delt cards
+# separate delt cards to dealers and players
+
+# increase the sum of players cards
+# increase the sum of dealers cards
+
+#display winner and ask if the player wants to keep playing
+
+#In an ideal world, player could place a bet thats going to be written in a bet amount txt file.
