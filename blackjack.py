81ffb0ff08a3df38c892f4b9324696bcab79e001
aleksanterif <aleksanteri.fagerholm@gmail.comgit config --global user.email aleksanteri.fagerholm@gmail.com>
2020-05-10 12:31:36 +0300

    created values for suits (maat) and ranks (arvot) also gave them values and created a card class

--- blackjack.py
+++ blackjack.py
@@ -1,12 +1,33 @@
-#random will offer me a great way to randomly select cards
+#random will offer me a great way to randomly select cards and shuffle deck
 import random
 
- # Player cards are an empty array in the beginning
-player_cards = []
- # Dealer cards are an empty array in the beginning
-dealer_cards = []
+ # Players hand is an empty array in the beginning
+player_hand = []
+ # Dealers hand is an empty array in the beginning
+dealer_hand = []
+
+# We should be able to tell if the user is playing
+userPlaying = True
 
 # object in which I define the values of the cards
+suits = ('Spades','Diamonds', 'Clubs', 'Hearts')
+ranks = ('Ace','Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King')
+values = {'Ace':11, 'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 
+            'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10}
+
+class Card:
+    # The __init__() function is called automatically every time the class is being used to create a new object.
+    # The self keyword gives us the access for the attributes and methods of the class
+    def __init__(self,suit,rank):
+        self.suit = suit
+        self.rank = rank
+    # the card should be output like "Ace of Spades"
+    def __str__(self):
+        return self.suit + ' of ' + self.rank
+
+test = Card( 'Ace', 'Spades' )
+print(test)
+# I need a deck class inwhich I can store my cards accordignly 
 # I need a function to deal the cards
 # Other funcion to display the delt cards
 # separate delt cards to dealers and players
